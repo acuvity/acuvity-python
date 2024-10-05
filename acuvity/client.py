7ea4d4f1e34324c97944b4daabccc798c56df52f
Marcus Heese <marcus@acuvity.ai>
2024-10-04 18:17:38 -0700

    first initial validate API done which supports messages and files

--- acuvity/client.py
+++ acuvity/client.py
@@ -1,18 +1,20 @@
+import base64
+import os
 import httpx
 import jwt
-import os
 
+from typing import Iterable, List, Any, Dict, Sequence, Union, Optional
 from urllib.parse import urlparse
 
 class AcuvityClient:
     def __init__(
             self,
             *,
-            token: str | None = None,
-            namespace: str | None = None,
-            api_url: str | None = None,
-            apex_url: str | None = None,
-            http_client: httpx.Client | None = None,
+            token: Optional[str] = None,
+            namespace: Optional[str] = None,
+            api_url: Optional[str] = None,
+            apex_url: Optional[str] = None,
+            http_client: Optional[httpx.Client] = None,
     ):
         """
         Initializes a new Acuvity client. At a minimum you need to provide a token, which can get passed through an environment variable.
@@ -25,8 +27,37 @@ class AcuvityClient:
         :param http_client: the HTTP client to use for making requests. If not provided, a new client will be created.
         """
 
+        # we initialize the available analyzers here as they are static right now
+        # this will need to change once they become dynamic, but even then we can cache them within the client
+        self._available_analyzers = {
+            "PIIs": [
+                "ner_detector",
+                "pii_detector",
+            ],
+            "Secrets": [
+                "secrets_detector",
+            ],
+            "Topics": [
+                "text_multi_classifier",
+                "text_classifier_corporate",
+            ],
+            "Exploits": [
+                "prompt_injection",
+                "harmful_content",
+                "jailbreak",
+            ],
+            "Languages": [
+                "language_detector",
+                "gibberish_detector",
+            ],
+        }
+
         # we initialize the client early as we might require it to fully initialize our own client
-        self.http_client = http_client if http_client is not None else httpx.Client()
+        self.http_client = http_client if http_client is not None else httpx.Client(
+            timeout=httpx.Timeout(timeout=600.0, connect=5.0),
+            limits=httpx.Limits(max_connections=1000, max_keepalive_connections=100),
+            follow_redirects=True,
+        )
 
         # token first, as we potentially need it to detect the other values
         if token is None:
@@ -99,7 +130,10 @@ class AcuvityClient:
             raise ValueError("Apex URL is not a valid URL: " + str(e))
 
     def orgsettings(self):
-        resp = self.http_client.get(
+        """
+        Retrieves the organization settings that the authenticated token belongs to.
+        """
+        resp_json = self.http_client.get(
             self.api_url + "/orgsettings",
             headers={
                 "Authorization": "Bearer " + self.token,
@@ -108,8 +142,121 @@ class AcuvityClient:
                 "Content-Type": "application/json; charset=utf-8",
             },
         )
-        resp_json = resp.json()
-        return resp_json[0]
+        if  resp_json.status_code != 200:
+            raise ValueError(f"failed to call orgsettings API: HTTP {resp_json.status_code}: {resp_json.text}")
+        resp = resp_json.json()
+
+        # we know this is a singleton, so there will always be exactly one
+        return resp[0]
+
+    def validate(
+            self,
+            *messages: str,
+            files: Union[Sequence[Union[str,os.PathLike]], os.PathLike, str, None] = None,
+            type: str = "Input",
+            analyzers: Optional[List[str]] = None,
+            annotations: Optional[Dict[str, str]] = None,
+            bypass_hash: Optional[str] = None,
+            anonymization: Optional[str] = None,
+    ) -> Any:
+        """
+        """
+        data = {}
+        # messages must be strings
+        for message in messages:
+            if not isinstance(message, str):
+                raise ValueError("messages must be strings")
+        if len(messages) == 0 and files is None:
+            raise ValueError("no messages and no files provided")
+        if len(messages) > 0:
+            data["messages"] = [message for message in messages]
+
+        # files must be a list of strings (or paths) or a single string (or path)
+        extractions = []
+        if files is not None:
+            process_files = []
+            if isinstance(files, str):
+                process_files.append(files)
+            elif isinstance(files, os.PathLike):
+                process_files.append(files)
+            elif isinstance(files, Iterable):
+                for file in files:
+                    if not isinstance(file, str) and not isinstance(file, os.PathLike):
+                        raise ValueError("files must be strings or paths")
+                    process_files.append(file)
+            else:
+                raise ValueError("files must be strings or paths")
+            for process_file in process_files:
+                with open(process_file, 'rb') as file:
+                    file_content = file.read()
+                    encoded_content = base64.b64encode(file_content).decode('utf-8')
+                    extractions.append({
+                        "content": encoded_content,
+                    })
+        if len(extractions) > 0:
+            data["extractions"] = extractions
+
+        # type must be either "Input" or "Output"
+        if type != "Input" and type != "Output":
+            raise ValueError("type must be either 'Input' or 'Output'")
+        data["type"] = type
+
+        # analyzers must be a list of strings
+        if analyzers is not None:
+            if not isinstance(analyzers, List):
+                raise ValueError("analyzers must be a list")
+            for analyzer in analyzers:
+                if not isinstance(analyzer, str):
+                    raise ValueError("analyzers must be strings")
+                if not analyzer.startswith(("+", "-")):
+                    raise ValueError("analyzers does not start with '+' or '-' to indicate inclusion or exclusion: " + analyzer)
+            data["analyzers"] = analyzers
+
+        # annotations must be a dictionary of strings
+        if annotations is not None:
+            if not isinstance(annotations, dict):
+                raise ValueError("annotations must be a dictionary")
+            for key, value in annotations.items():
+                if not isinstance(key, str) or not isinstance(value, str):
+                    raise ValueError("annotations must be strings")
+            data["annotations"] = annotations
+
+        # bypass_hash must be a string
+        if bypass_hash is not None:
+            if not isinstance(bypass_hash, str):
+                raise ValueError("bypass_hash must be a string")
+            data["bypass"] = bypass_hash
+
+        # anonymization must be "FixedSize" or "VariableSize"
+        if anonymization is not None:
+            if anonymization != "FixedSize" and anonymization != "VariableSize":
+                raise ValueError("anonymization must be 'FixedSize' or 'VariableSize'")
+            data["anonymization"] = anonymization
+
+        resp_json = self.http_client.post(
+            self.apex_url + "/_acuvity/validate",
+            headers={
+                "Authorization": "Bearer " + self.token,
+                "X-Namespace": self.namespace,
+                "Accept": "application/json",
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            json=data,
+        )
+        if  resp_json.status_code != 200:
+            raise ValueError(f"failed to call validate API: HTTP {resp_json.status_code}: {resp_json.text}")
+
+        resp = resp_json.json()
+        return resp
+
+    def list_analyzer_groups(self) -> List[str]:
+        return list(self._available_analyzers.keys())
+    
+    def list_analyzers(self, group: str | None = None) -> List[str]:
+        if group is None:
+            return [analyzer for analyzers in self._available_analyzers.values() for analyzer in analyzers]
+        return self._available_analyzers[group]
+
 
 # TODO: implement async client as well
 #class AsyncAcuvityClient:
