354cbba75cca82eac92a3a30924b2d395dfa51b3
Satyam Sinha <satyam@acuvity.ai>
2025-01-17 12:40:21 -0800

    remove guardname_analyzer_id_map

--- src/acuvity/response/constants.py
+++ src/acuvity/response/constants.py
@@ -7,25 +7,6 @@ from acuvity.models.textualdetection import TextualdetectionType
 # Default action for guards
 DEFAULT_ACTION = "deny"
 
-guardname_analyzer_id_map: Dict[GuardName, str] = {
-    # Exploit guards
-    GuardName.PROMPT_INJECTION: 'en-text-prompt_injection-detector',
-    GuardName.JAIL_BREAK: 'en-text-jailbreak-detector',
-    GuardName.MALICIOUS_URL: 'url-malicious-detector',
-
-    # Topic guards with prefixes
-    GuardName.TOXICITY: 'en-text-toxicity-detector',
-    GuardName.BIAS: 'en-text-bias-detector',
-    GuardName.HARMFUL_CONTENT: 'en-text-harmful-content-detector',
-
-    # Other guards
-    GuardName.LANGUAGE: 'text-language-classifier',
-    GuardName.PII_DETECTOR: 'en-text-ner-detector',
-    GuardName.SECRETS_DETECTOR:'text-pattern-detector',
-    GuardName.KEYWORD_DETECTOR: 'text-keyword-detector',
-    GuardName.MODALITY: 'modality-detector',
-}
-
 class GuardType(Enum):
     """Types of guards and their corresponding sections in Extraction."""
     EXPLOIT = auto()         # Direct access in exploits
