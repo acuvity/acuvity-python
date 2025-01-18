09ba9abbea942d44942615c2a4d9c5739cf63e71
Satyam Sinha <satyamsi@users.noreply.github.com>
2025-01-17 18:04:40 -0800

    Examples (#25)
    * Create examples which are smaller in size and easy to comprehend and execute
    * Add a workflow to install, run examples and test

---
+++ examples/guards.py
@@ -0,0 +1,27 @@
+import os
+
+from rich import print
+
+import acuvity
+from acuvity import Acuvity
+from acuvity.guard.constants import GuardName
+
+s = Acuvity(
+    # not required at all if set in environment variables
+    security=acuvity.Security(
+       token=os.getenv("ACUVITY_TOKEN", ""),
+    )
+)
+
+# This is to show the list of available guards supported by Acuvity.
+print("--------------------------------------------------------------------------------")
+print("Avaiable Guards and Categories: ")
+print("--------------------------------------------------------------------------------")
+for g in s.apex.list_available_guards():
+    print(f" - {g}")
+    if g == str(GuardName.PII_DETECTOR):
+        for pii in s.apex.list_detectable_piis():
+            print(f"     - {pii}")
+    elif g == str(GuardName.SECRETS_DETECTOR):
+        for secrets in s.apex.list_detectable_secrets():
+            print(f"     - {secrets}")
---
+++ examples/intermediate_multimodal.py
@@ -0,0 +1,62 @@
+import os
+
+from rich import print
+
+import acuvity
+from acuvity import Acuvity
+
+s = Acuvity(
+    # not required at all if set in environment variables
+    security=acuvity.Security(
+       token=os.getenv("ACUVITY_TOKEN", ""),
+    )
+)
+
+print("--------------------------------------------------------------------------------")
+print("Scenario: multimodal with multiple text prompts and file with guard config in a variable")
+input_messages = [
+    "corporate sales number are 10k filling, in.abcd@gmail.com, 123abcd@yahoo.com hate you, 792-77-3459, 792-77-3453, 792-77-3454",
+    "hello how are you",
+]
+file="./test_data/pi-test.txt"
+guard_config = {
+    "guardrails": [
+        {
+            "name": "prompt_injection",
+            "threshold": ">= 0.2"
+        },
+        {
+            "name": "jailbreak",
+            "threshold": ">= 0.7"
+        },
+        {
+            "name": "malicious_url",
+            "threshold": ">= 0.7"
+        },
+        {
+        "name": "pii_detector",
+        "count_threshold": 4,
+        },
+        {
+            "name": "secrets_detector"
+        },
+        {
+            "name": "biased"
+        },
+        {
+            "name": "toxic"
+        },
+        {
+            "name": "language"
+        }
+    ]
+}
+matches = s.apex.scan(*input_messages, files=file, guard_config=guard_config).matches()
+print("Input:\n", input_messages, file)
+print("Matches:\n", matches)
+
+print("--------------------------------------------------------------------------------")
+print("Scenario: multimodal with multiple text prompts and file with guard config")
+matches = s.apex.scan(*input_messages, files=file, guard_config="./configs/simple_default_guard_config.yaml").matches()
+print("Input:\n", input_messages)
+print("Matches:\n", matches)
