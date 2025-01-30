a51af68b7b7a5f0758a9781e5b52132a9d4b0eae
Abhijit Herekar <in.abhijit@gmail.com>
2025-01-30 13:43:03 -0800

    revert the version.

--- src/acuvity/_version.py
+++ src/acuvity/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "acuvity"
-__version__: str = "0.5.6"
+__version__: str = "0.5.5"
 __openapi_doc_version__: str = "1.0"
-__gen_version__: str = "2.498.4"
-__user_agent__: str = "speakeasy-sdk/python 0.5.6 2.498.4 1.0 acuvity"
+__gen_version__: str = "2.499.0"
+__user_agent__: str = "speakeasy-sdk/python 0.5.5 2.499.0 1.0 acuvity"
 
 try:
     if __package__ is not None:
